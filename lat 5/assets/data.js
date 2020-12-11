86748937f0378c32cef4f95fe2c0e34ae9ee7968
nivellekisela <70366966+nivellekisela@users.noreply.github.com>
2020-12-11 13:56:58 +0700

    Add files via upload

---
+++ lat 5/assets/data.js
@@ -0,0 +1,10 @@
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "188px";
+  document.getElementById("main").style.marginLeft = "188px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
